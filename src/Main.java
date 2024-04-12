4941902f6cefd313ff02e2c2705e54c653a0e75a
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-12 21:42:19 +0700

    draft login

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
-public class Main {
+import javax.swing.*;
+public class Main extends JFrame{
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        new login();
     }
 }
---
+++ src/login.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+
+
+public class login extends JFrame {
+    private JPanel login;
+    private JLabel dangnhap_label;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JCheckBox ghiNhớTôiCheckBox;
+    private JButton đăngNhậpButton;
+    private JButton chưaĐăngKýTạoButton;
+    private JButton đăngNhậpVớiGoogleButton;
+    private JButton quênMậtKhẩuButton;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
