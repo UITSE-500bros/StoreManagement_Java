bcb5d7a5e1d84d96490f9bc41940190f74f90b38
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-04-16 21:25:14 +0700

    devil fruit

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main extends JFrame{
     public static void main(String[] args) {
         new login();
         System.out.println("Testing");
+        System.out.println("One piece");
     }
 }
