6114dba2284dfaf0dbeb7955ae55c7458b1dd056
Duong Thuan Tri <duongthuantri159@gmail.com>
2024-04-19 13:52:39 +0700

    Remove all files

--- src/Main.java
+++
@@ -1,9 +0,0 @@
-import javax.swing.*;
-public class Main extends JFrame{
-
-    public static void main(String[] args) {
-        new login();
-        System.out.println("Testing");
-        System.out.println("One piece");
-    }
-}
