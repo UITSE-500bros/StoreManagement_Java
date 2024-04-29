1f4de5600af9ca5f0c583fbd6bd2b10e28b528ed
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-04-29 16:04:20 +0700

    feat: init class

--- src/UI/ContainerPanel.java
+++ src/UI/ContainerPanel.java
@@ -1,5 +1,28 @@
 package UI;
 
-public class ContainerPanel {
+import java.awt.Color;
+import java.awt.GradientPaint;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
+import javax.swing.JPanel;
+
+public class ContainerPanel extends JPanel {
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Color color1 = Color.decode("#F0E6DF");
+    private Color color2 = Color.decode("#C7BEB7");
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        int width = getWidth();
+        int height = getHeight();
+        GradientPaint gp = new GradientPaint(0, 0, color1, 0, height, color2);
+        g2d.setPaint(gp);
+        g2d.fillRect(0, 0, width, height);
+    }
 }
--- src/UI/CustomPanel.java
+++ src/UI/CustomPanel.java
@@ -1,5 +1,48 @@
 package UI;
 
-public class CustomPanel {
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 
+import javax.swing.JPanel;
+
+public class CustomPanel extends JPanel {
+    
+	private static final long serialVersionUID = 1L;
+	private int cornerRadius;
+    private Color backgroundColor;
+    private Color strokeColor;
+    private Color shadowColor;
+    private int strokeWidth;
+
+    public CustomPanel() {
+        setOpaque(false);
+        cornerRadius = 30;
+        backgroundColor = new Color(255,249,243, 180);
+        strokeColor = new Color(192, 192, 192);
+        strokeWidth = 1;
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+    	int width = getWidth();
+        int height = getHeight();
+        Graphics2D graphics = (Graphics2D) g;
+        graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
+        
+
+        // Draws the rounded opaque panel with borders.
+        graphics.setColor(backgroundColor);
+        graphics.fillRoundRect(0, 0, width, height, cornerRadius, cornerRadius); //paint background
+
+        // Draws the stroke
+        graphics.setColor(strokeColor);
+        graphics.setStroke(new BasicStroke(strokeWidth));
+        graphics.drawRoundRect(0, 0, width-1, height-1, cornerRadius, cornerRadius); //paint border
+
+        super.paintComponent(g);
+    }
 }
