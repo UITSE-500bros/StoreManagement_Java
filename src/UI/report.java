723e55049152d3dc5ad11e27d7d84c264f8d7189
ThInhHuynh <22521406@gm.uit.edu.vn>
2024-05-05 10:54:29 +0700

    feat: report page

---
+++ src/UI/report.java
@@ -0,0 +1,108 @@
+package UI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.BorderLayout;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JComboBox;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.formdev.flatlaf.FlatLightLaf;
+
+import javax.swing.JScrollPane;
+
+public class report extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTable table;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					UIManager.setLookAndFeel(new FlatLightLaf());
+					report frame = new report();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public report() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 731, 538);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.NORTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JButton btnNewButton = new JButton("");
+		btnNewButton.setPreferredSize(new Dimension(30, 30));
+		btnNewButton.setIcon(new ImageIcon(report.class.getResource("/resource/searchStoreIcon 1.png")));
+	
+		panel.add(btnNewButton, BorderLayout.WEST);
+		
+		textField = new JTextField();
+		textField.setMinimumSize(new Dimension(24, 29));
+		textField.setPreferredSize(new Dimension(300, 30));
+		panel.add(textField, BorderLayout.CENTER);
+		textField.setColumns(10);
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setPreferredSize(new Dimension(150, 30));
+		String[] reportType = {"Báo cáo công nợ", "Báo cáo hàng tồn"};
+		for (String type : reportType) {
+			comboBox.addItem(type);
+		}
+		
+		panel.add(comboBox, BorderLayout.EAST);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		table.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"STT", "\u0110\u1EA1i l\u00FD", "S\u1ED1 phi\u1EBFu xu\u1EA5t", "T\u1ED5ng gi\u00E1 tr\u1ECB", "T\u1EF7 l\u1EC7"
+			}
+		) {
+			boolean[] columnEditables = new boolean[] {
+				true, false, true, true, true
+			};
+			public boolean isCellEditable(int row, int column) {
+				return columnEditables[column];
+			}
+		});
+		scrollPane.setViewportView(table);
+	}
+}
