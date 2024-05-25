8a931e9366ade29a8fc2cc712dc9d0573ac0ec2c
DPhngNam <22520908@gm.uit.edu.vn>
2024-05-25 15:39:37 +0700

    fix: create export file

--- src/main/java/Controller/DaiLyController.java
+++ src/main/java/Controller/DaiLyController.java
@@ -13,6 +13,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.lang.reflect.Type;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DaiLyController {
@@ -20,12 +21,13 @@ public class DaiLyController {
     private List<daily> dailyList;
     public DaiLyController() {
         connection = new DaiLyRepository();
+        dailyList = new ArrayList<>();
     }
     public List<daily> showDaiLy() throws IOException {
         String link = "daily/getAllDaiLy";
         connection.openGetConnection(link);
         dailyList = connection.getAllDaiLy();
-        return dailyList;
+         return dailyList;
     }
     public String addNewDaiLy(daily daily, quan quan, loaidaily loaidaily){
         return connection.insertDaiLy(daily);
--- src/main/java/Repository/DaiLyRepository.java
+++ src/main/java/Repository/DaiLyRepository.java
@@ -4,14 +4,10 @@ import Models.daily;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
-
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.lang.reflect.Type;
 import java.util.List;
-import java.io.*;
 public class DaiLyRepository extends Connection{
 
     public DaiLyRepository() {
@@ -28,33 +24,11 @@ public class DaiLyRepository extends Connection{
         return response;
     }
     public List<daily> getAllDaiLy() throws IOException {
-        Gson gson = new Gson();
+        gson = new Gson();
         JsonReader reader = new JsonReader(new InputStreamReader(con.getInputStream()));
         Type listType = new TypeToken<List<daily>>(){}.getType();
         List<daily> dailies = gson.fromJson(reader, listType);
         con.disconnect();
         return dailies;
-    }
-    
-    public int getSoNoDuocNoThem(int madaily) throws IOException {
-
-        openGetConnection("daily/getSoNoDuocNoThem/" + madaily);
-        response = "";
-        if (response.contains("201")) {
-            BufferedReader in = new BufferedReader(new InputStreamReader(getCon().getInputStream()));
-            String inputLine;
-            StringBuffer content = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                content.append(inputLine);
-            }
-            in.close();
-            closeConnection();
-            return Integer.parseInt(content.toString()); // Convert the response body to int
-        } else {
-            closeConnection();
-
-            throw new IOException("Failed to create phieuxuathang. HTTP response code: " + response);
-        }
-
     }
 }
